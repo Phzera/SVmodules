fcd21f7bfe18e73a12d6fc250baaeca4d54318fe
Pedro Oliveira <ph.telecom@outlook.com>
2023-06-05 13:44:10 -0300

    Update shift register testbench

--- shift_register/tb_shiftreg.sv
+++ shift_register/tb_shiftreg.sv
@@ -8,23 +8,25 @@
 */
 
 module tb_shiftreg;
-
+   parameter MSB;
    logic           tb_clk;
    logic           tb_rstn;
    logic           tb_en;    // shift-register on/off
    logic           tb_dir;   // left or right shift
    logic           tb_data;  // Input data for the first flop
-   wire [MSB-1:0]  out;   // Output read currennt value of all flops
-
+   logic [MSB-1:0] out;   // Output read currennt value of all flops
+        
     // Instatiate design
-   shift_register #(MSB) srefg_u (
-    .clk_i(tb_clk),
-    .rstn_i(tb_rstn),
-    .en_i(tb_en),  
-    .dir_i(tb_dir), 
-    .data_i(tb_data),
-    .data_o(out)
-   )
+   shift_register #(
+       .MSB(MSB)
+   ) shift_reg_u (
+       .clk_i  (tb_clk),
+       .rstn_i (tb_rstn),
+       .en_i   (tb_en),  
+       .dir_i  (tb_dir), 
+       .data_i (tb_data),
+       .data_o (out)
+   );
 
    // Generate clock @50 MHz
    always #10 tb_clk = ~tb_clk;
@@ -33,8 +35,8 @@ module tb_shiftreg;
     initial begin
         tb_clk  <= 'b0;
         tb_rstn <= 'b1;
-        tb_en;  <= 'b0;
-        tb_dir; <= 'b0;
+        tb_en   <= 'b0;
+        tb_dir  <= 'b0;
         tb_data <= 'b1;
     end
 
@@ -45,7 +47,10 @@ module tb_shiftreg;
         #20
         tb_rstn <= 'b1;
         tb_en <= 'b1;
-
+        
+        // Monitor variables and print for debug
+        $monitor("rstn=%0b, data=%0b, en=%0b, dir=%0b, out=%b", tb_rstn, tb_data, tb_en, tb_dir, out);
+        
         // Drive alternate values to data pin for 8 clock cycles
         repeat(8) @ (posedge tb_clk)
             tb_data <= ~tb_data;
@@ -57,11 +62,8 @@ module tb_shiftreg;
             tb_data <= ~tb_data;
         
         // Drive nothing for 5 clock cycles
-        repeat (5) (@posedge tb_clk)
+        repeat (5) @ (posedge tb_clk)
         $finish;
     end
-
-    // Monitor variables and print for debug
-    $monitor("rstn=%0b, data=%0b, en=%0b, dir=%0b, out=%b", tb_rstn, tb_data, tb_en, tb_dir, out);
 
 endmodule
